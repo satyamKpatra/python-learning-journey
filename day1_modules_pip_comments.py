f08c8cbc1f56424ed34078df710c3d4df34c5d0e
Satyam Kumar Patra <satyamkumarpatra790@gmail.com>
2025-12-24 09:15:40 +0530

    Create day1_modules_pip_comments.py

---
+++ day1_modules_pip_comments.py
@@ -0,0 +1,9 @@
+# Day 1 of Python Learning
+# Topic: Modules, pip, Comments
+
+import math
+
+print("Python Learning Day 1")
+print("Square root of 16 is:", math.sqrt(16))
+
+# This program demonstrates how to use modules in Python
